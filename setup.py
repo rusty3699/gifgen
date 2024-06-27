5d2ab0b33c1397c195b649cb753b50fc7585b062
Anish Tipnis <22395725+rusty3699@users.noreply.github.com>
2024-06-27 10:01:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gifgen',
-    version='1.2.0',
+    version='1.3.0',
     packages=find_packages(),
     install_requires=[
         'Pillow',
